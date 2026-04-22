c069efa9f26942cdb0dff2b7b310e186e19bfe11
Ujvala Chavan <ujjwalasure1991@gmail.com>
2026-04-22 07:26:10 +0000

    Added Subtractor and SubtractorTest

---
+++ src/Subtractor.java
@@ -0,0 +1,5 @@
+public class Subtractor {
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ tst/SubtractorTest.java
@@ -0,0 +1,6 @@
+public class SubtractorTest {
+    public static void main(String[] args) {
+        Subtractor s = new Subtractor();
+        System.out.println(s.subtract(10, 3));
+    }
+}
